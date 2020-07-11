18e6211e8426b24fa0c98a0da4480ade462a3592
AndIReyf <andrey.prudius@hotmail.com>
2020-07-11 12:40:12 +0200

    Updates

---
+++ precache-manifest.5c45600455fb1347abf0fe38dbd2cdfe.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af88c4a8f20da140ab35af915846b8ac",
+    "url": "/React_Homework/index.html"
+  },
+  {
+    "revision": "21fa395c5410d95b0bcb",
+    "url": "/React_Homework/static/css/main.06ac5f7c.chunk.css"
+  },
+  {
+    "revision": "7dc63a9cd839f826135d",
+    "url": "/React_Homework/static/js/2.7144bdf8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React_Homework/static/js/2.7144bdf8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "21fa395c5410d95b0bcb",
+    "url": "/React_Homework/static/js/main.ac07c44f.chunk.js"
+  },
+  {
+    "revision": "a2076ea9b5c99ad10f8c",
+    "url": "/React_Homework/static/js/runtime-main.cc4a4677.js"
+  },
+  {
+    "revision": "c3f042ec3c83958885ec2384061fdf53",
+    "url": "/React_Homework/static/media/avatar.c3f042ec.jpeg"
+  }
+]);
