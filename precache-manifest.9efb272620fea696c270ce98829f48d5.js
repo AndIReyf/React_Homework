1f2b4a8916fdc01bd859458c9db5cdeab12b0861
AndIReyf <andrey.prudius@hotmail.com>
2020-09-11 19:02:57 +0200

    Updates

---
+++ precache-manifest.9efb272620fea696c270ce98829f48d5.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "905b3547c6ba54584055b74731afc6fa",
+    "url": "/React_Homework/index.html"
+  },
+  {
+    "revision": "817f3589703be93b0d3f",
+    "url": "/React_Homework/static/css/main.7d4fa427.chunk.css"
+  },
+  {
+    "revision": "0c2c6e8299a6322e21de",
+    "url": "/React_Homework/static/js/2.fd5b2d5b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/React_Homework/static/js/2.fd5b2d5b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "817f3589703be93b0d3f",
+    "url": "/React_Homework/static/js/main.0cdcd484.chunk.js"
+  },
+  {
+    "revision": "a2076ea9b5c99ad10f8c",
+    "url": "/React_Homework/static/js/runtime-main.cc4a4677.js"
+  },
+  {
+    "revision": "16ee53fb0cd7bb51f3c9148be0e850e1",
+    "url": "/React_Homework/static/media/2.16ee53fb.svg"
+  },
+  {
+    "revision": "c3f042ec3c83958885ec2384061fdf53",
+    "url": "/React_Homework/static/media/avatar.c3f042ec.jpeg"
+  },
+  {
+    "revision": "01f2f478ef4658fccd4390f40ac94244",
+    "url": "/React_Homework/static/media/icomoon.01f2f478.svg"
+  },
+  {
+    "revision": "66f129014a79d0fa32a99c57a290a46e",
+    "url": "/React_Homework/static/media/icomoon.66f12901.woff"
+  },
+  {
+    "revision": "b3106dd2c08cc21b54373dfa2e4b0049",
+    "url": "/React_Homework/static/media/icomoon.b3106dd2.ttf"
+  },
+  {
+    "revision": "fff51f4589858967a81f3f423417b1cb",
+    "url": "/React_Homework/static/media/icomoon.fff51f45.eot"
+  }
+]);
